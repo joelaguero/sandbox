52462118e5cc5ab884a7cbe8e1baa963e79ddb17
Joel Aguero <joelmaguero@gmail.com>
2016-05-27 10:57:05 -0400

    Implement untested solution

--- toy-problems/reverse-vowels.cc
+++ toy-problems/reverse-vowels.cc
@@ -7,16 +7,30 @@ Given s = "hello", return "holle".
 Example 2:
 Given s = "leetcode", return "leotcede".
 */
+#include <vector>;
 
 class Solution {
 public:
   string reverseVowel(string s) {
+    std::vector<char> vowels = ['a', 'e', 'i', 'o', 'u'];
     // create a storage array of ints respresenting vowel indices
-
+    std::vector<int> indices;
     // iterate over the string and stop when you find a vowel
     // add the index of the vowel to the storage array
-
+    for (int i = 0; i < s.length(); i++) {
+      if(std::find(vowels.begin(), vowels.end(), s[i]) != vowels.end()) {
+        indices.push_back(i);
+      }
+    }
     // iterate over half the storage array from the front and end using the iterator variable
     // before incrementing, swap the values at the location in the original string using a helper function that takes two indices and the original string
+    for (int j = 0; j < indices.size()/2 - 1; j++) {
+      int front = indices[j];
+      int back = indices[indices.size() - 1 - j];
+      char temp = s[front];
+      s[front] = s[back];
+      s[back] = temp;
+    }
+    return s;
   }
 };
