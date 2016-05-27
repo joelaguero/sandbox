d6e17e4eca6b198a1932776f5438d54a1ef3e8bd
Joel Aguero <joelmaguero@gmail.com>
2016-05-27 10:31:39 -0400

    Write pseudocode for solution

---
+++ toy-problems/reverse-vowels.cc
@@ -0,0 +1,22 @@
+/*
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+*/
+
+class Solution {
+public:
+  string reverseVowel(string s) {
+    // create a storage array of ints respresenting vowel indices
+
+    // iterate over the string and stop when you find a vowel
+    // add the index of the vowel to the storage array
+
+    // iterate over half the storage array from the front and end using the iterator variable
+    // before incrementing, swap the values at the location in the original string using a helper function that takes two indices and the original string
+  }
+};
