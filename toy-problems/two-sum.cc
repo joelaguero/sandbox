0c1fb78d690d015afc087aa9addd0f424ca7ca89
Joel Aguero <joelmaguero@gmail.com>
2016-05-28 10:26:49 -0400

    Write pseudocode

---
+++ toy-problems/two-sum.cc
@@ -0,0 +1,17 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+*/
+class Solution {
+public:
+  std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    // create an empty set
+    // at each index, add the number to the set if it's less than the target
+
+    // at each index, check if (target - current number) is in the set
+    // if it is, return a tuple with current number and delta
+
+    // else return an empty vector
+  }
+};
