2c1ec107ae9f9a6ce03767c1ddb05571ea01cf59
Joel Aguero <joelmaguero@gmail.com>
2016-05-30 10:14:36 -0400

    Create flatten binary tree to linked list

---
+++ toy-problems/flatten-binary-tree-linked-list.cc
@@ -0,0 +1,18 @@
+/**
+ * Given a binary tree, flatten it to a linked list in-place.
+ *
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+      // visit current node
+      
+    }
+};
