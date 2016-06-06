22b2bbe1b02011dd57f281b5d2172c61e7f1f793
Joel Aguero <joelmaguero@gmail.com>
2016-06-05 18:13:38 -0700

    Write pseudocode for solution

--- toy-problems/design-stack.cc
+++ toy-problems/design-stack.cc
@@ -1,8 +1,20 @@
-*/
-Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+//
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// getMin() -- Retrieve the minimum element in the stack.
+class Solution {
+public:
+  // store the stack in an array
+  // PUSH: write function that adds to end of array
 
-push(x) -- Push element x onto stack.
-pop() -- Removes the element on top of the stack.
-top() -- Get the top element.
-getMin() -- Retrieve the minimum element in the stack.
-*/
+  // POP: write function that removes and returns item at end of array
+
+  // TOP: return the last element in the array
+
+  // GETMIN: have the index of the minimum element (updated on push/pop)
+  // pops items until it gets to the item it's looking for
+  // TODO: how can we access the minimum in constant time, despite adding and removing elements
+  // (which would change the minimum)
+}
