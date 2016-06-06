99a31901bffe3f3a56a4c833cb692f09ace9c3c9
Joel Aguero <joelmaguero@gmail.com>
2016-06-06 14:44:07 -0700

    Implement untested stack

--- toy-problems/design-stack.cc
+++ toy-problems/design-stack.cc
@@ -7,14 +7,43 @@
 class Solution {
 public:
   // store the stack in an array
+  // store another stack that tracks the minimum at each level of the stack
+  std::vector<int> stack;
+  std::vector<int> minimum;
+
   // PUSH: write function that adds to end of array
+  // if the value is greater than top of minStack then push same value to min stack
+  // else push the top of the minStack to the minStack (duplicate it)
+  void push(int value) {
+    stack.push_back(value);
+    if (value < minimum[minimum.length() - 1]) {
+      minimum.push_back(value);
+    }
+  }
 
   // POP: write function that removes and returns item at end of array
+  // also pop the item from the minStack
+  int pop() {
+    if (stack.length() == 0) { return null; }
+
+    int top = stack[stack.length() - 1];
+    if (top < minimum[minimum.length() - 1]) {
+      minimum.erase(minimum.length() - 1);
+    }
+    return top;
+  }
 
   // TOP: return the last element in the array
+  int top() {
+    if (stack.length() = 0) { return null; }
 
-  // GETMIN: have the index of the minimum element (updated on push/pop)
-  // pops items until it gets to the item it's looking for
-  // TODO: how can we access the minimum in constant time, despite adding and removing elements
-  // (which would change the minimum)
+    return stack[stack.length - 1];
+  }
+
+  // GETMIN: return the top of the minStack
+  int getMin() {
+    if (stack.length() = 0) { return null; }
+
+    return minimum[minimum.length - 1];
+  }
 }
