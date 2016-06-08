73e5a8d90560281f56e7c7850d28f92e12cecbb1
Joel Aguero <joelmaguero@gmail.com>
2016-06-08 10:32:00 -0700

    Add duplicates problem

---
+++ toy-problems/contains-nearby-almost-duplicate.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+    // t = delta of values at indices
+    // k = delta of indices
+    // conditions: nums[i] - nums[j] (abs) <= t
+    //             i - j (abs) <= k
+    
+};
