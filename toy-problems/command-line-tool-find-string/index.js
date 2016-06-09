84c8e3c36fdd090a723732bc4a4d7bf1401269c1
Joel Aguero <joelmaguero@gmail.com>
2016-06-08 21:53:18 -0700

    Add explanation of use of exec method

--- toy-problems/command-line-tool-find-string/index.js
+++ toy-problems/command-line-tool-find-string/index.js
@@ -6,6 +6,8 @@ var target = customArgs[0];
 console.log(target);
 
 var exec = require('child_process').exec; // child_process is a module that comes with Node, exec is a method on this built-in module "child_process"
+
+// this uses the Unix commands ls and grep to list all the files and pass them to grep to search for the pattern
 var child = exec('ls -a | grep ' + target, function(err, stdout, stderr) {
   console.log(stdout);
 });
