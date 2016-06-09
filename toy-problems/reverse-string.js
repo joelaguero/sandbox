47396edea23c0b0334a5f39ab0b2e2faa8cb3ff0
Joel Aguero <joelmaguero@gmail.com>
2016-06-08 22:27:13 -0700

    Remove str duplication for constant space

--- toy-problems/reverse-string.js
+++ toy-problems/reverse-string.js
@@ -13,7 +13,6 @@
 // 01234
 
 const reverseString = (str) => {
-  let result = str;
   if (str.length >= 2) {
     for (let i = 0; i < Math.floor(str.length / 2); i++) {
       let temp = str[i];
@@ -21,5 +20,5 @@ const reverseString = (str) => {
       str[str.length - 1 - i] = temp;
     }
   }
-  return result;
+  return str;
 }
