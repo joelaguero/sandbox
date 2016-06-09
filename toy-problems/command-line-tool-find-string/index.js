74d287e8c91d4660260a262af4dcac78e4276091
Joel Aguero <joelmaguero@gmail.com>
2016-06-08 21:49:39 -0700

    Implement command line tool for finding files with string

--- toy-problems/command-line-tool-find-string/index.js
+++ toy-problems/command-line-tool-find-string/index.js
@@ -4,3 +4,8 @@ var customArgs = process.argv.slice(2);
 var target = customArgs[0];
 
 console.log(target);
+
+var exec = require('child_process').exec;
+var child = exec('ls -a | grep ' + target, function(err, stdout, stderr) {
+  console.log(stdout);
+});
