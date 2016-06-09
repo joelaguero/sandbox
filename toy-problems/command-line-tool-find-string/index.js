caf532a615b57b96519dec8f9287a3b8a2286c85
Joel Aguero <joelmaguero@gmail.com>
2016-06-08 21:49:53 -0700

    Add comment to line 8

--- toy-problems/command-line-tool-find-string/index.js
+++ toy-problems/command-line-tool-find-string/index.js
@@ -5,7 +5,7 @@ var target = customArgs[0];
 
 console.log(target);
 
-var exec = require('child_process').exec;
+var exec = require('child_process').exec; // child_process is a module that comes with Node, exec is a method on this built-in module "child_process"
 var child = exec('ls -a | grep ' + target, function(err, stdout, stderr) {
   console.log(stdout);
 });
