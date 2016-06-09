8f058c46ab434807f954a45468da34a62de023ca
Joel Aguero <joelmaguero@gmail.com>
2016-06-08 21:54:18 -0700

    Remove console log

--- toy-problems/command-line-tool-find-string/index.js
+++ toy-problems/command-line-tool-find-string/index.js
@@ -3,8 +3,6 @@
 var customArgs = process.argv.slice(2);
 var target = customArgs[0];
 
-console.log(target);
-
 var exec = require('child_process').exec; // child_process is a module that comes with Node, exec is a method on this built-in module "child_process"
 
 // this uses the Unix commands ls and grep to list all the files and pass them to grep to search for the pattern
