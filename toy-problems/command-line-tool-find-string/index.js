6b964b14d718d6befea4e3aa0590459da8739836
Joel Aguero <joelmaguero@gmail.com>
2016-06-08 22:07:09 -0700

    Minor styling/comments

---
+++ toy-problems/command-line-tool-find-string/index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+var customArgs = process.argv.slice(2);
+var target = customArgs[0];
+
+var exec = require('child_process').exec; // child_process is a module that comes with Node, exec is a method on this built-in module "child_process"
+
+// this uses the Unix commands ls and grep to list all the files and pass them to grep to search for the pattern
+var child = exec('ls -a | grep ' + target, function(err, stdout, stderr) {
+  console.log(stdout);
+});
