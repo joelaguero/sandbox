edb311c870a15abcae78aa0d765451e5423d1b72
Joel Aguero <joelmaguero@gmail.com>
2016-06-19 17:23:56 -0700

    Create empty methods in App

--- battleship/src/components/App.jsx
+++ battleship/src/components/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import GameInfo from './GameInfo.jsx';
+import GameBoard from './GameBoard.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.boardSize = 10;
+    startNewGame();
+  }
+
+  startNewGame() {
+    this.setState({
+      playerTurn: 1,
+    });
+    randomlyPlaceShips();
+  }
+
+  randomlyPlaceShips() {
+    
+  }
+
   render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <GameInfo />
+        <GameBoard size={this.boardSize} ships={this.state.ships} />
+      </div>
     );
   }
 };
