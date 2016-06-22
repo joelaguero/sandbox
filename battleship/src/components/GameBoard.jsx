8eeed597af5985ceeeca909f752ba39ecd2fd787
Joel Aguero <joelmaguero@gmail.com>
2016-06-22 15:52:28 -0700

    Implement simple GameBoard

--- battleship/src/components/GameBoard.jsx
+++ battleship/src/components/GameBoard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const GameBoard = () => (
-  <div>
-    This is the game information.
-  </div>
-);
+const GameBoard = (props) => {
+  let grid = [];
+  for (var i = 0; i < props.size; i++) {
+    let cols = [];
+    for (var j = 0; j < props.size; j++) {
+      cols.push(<td/>);
+    }
+    grid.push(<tr>{cols}</tr>);
+  }
+
+  return (
+    <table>
+      {grid}
+    </table>
+  );
+};
 
 export default GameBoard;
